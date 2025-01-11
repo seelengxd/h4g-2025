2ea2f0a8ce8329a22c71dc42982ad47b381a9a51
seeleng <seelengxd@gmail.com>
2025-01-11 13:32:50 +0800

    feat(frontend): init api and router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import { StoreProvider } from "@/store/store-provider.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <StoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>hi</div>} />
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
+  </StrictMode>
+);

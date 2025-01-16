9cb25bafab395ccb86e837665de764795abae2be
seeleng <seelengxd@gmail.com>
2025-01-17 02:15:42 +0800

    feat: inventory summary report api

--- backend/src/reports/schemas.py
+++ backend/src/reports/schemas.py
@@ -2,12 +2,37 @@ from datetime import datetime
 from pydantic import BaseModel
 
 from src.auth.schemas import UserPublic
+from src.products.models import Category
 
 
 class MiniReportPublic(BaseModel):
-
     id: int
     generated_date: datetime
-    pdf_file_path: str # only store file path on local, and staff can only access report they generated
+    pdf_file_path: str  # only store file path on local, and staff can only access report they generated
     user_id: int
     user: UserPublic
+
+
+class InventorySummaryReportRowOrder(BaseModel):
+    order_id: int
+    created_at: datetime
+    user_id: int
+    full_name: str
+    qty: int
+
+
+class InventorySummaryReportRowDetails(BaseModel):
+    total: int
+    orders: list[InventorySummaryReportRowOrder]
+
+
+class InventorySummaryReportRow(BaseModel):
+    id: int
+    name: str
+    category: Category
+    image: str | None = None
+    total_qty: int
+
+    pending_approval: InventorySummaryReportRowDetails
+    pending_claim: InventorySummaryReportRowDetails
+    remaining: int

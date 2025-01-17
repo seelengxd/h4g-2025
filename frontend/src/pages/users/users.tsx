636525c23ae3e21d01f6337e1319974f8ea2b30c
Chloe Lim <chloelim22@gmail.com>
2025-01-17 23:17:34 +0800

    fix: i hate font-light

--- frontend/src/pages/users/users.tsx
+++ frontend/src/pages/users/users.tsx
@@ -15,7 +15,7 @@ const Users = () => {
     <>
       <div className="sticky top-0 bg-white">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-light">Manage residents</h1>
+          <h1 className="text-2xl font-medium">Manage residents</h1>
           <NewUserFormDialog>
             <Button>
               <UserPlus /> Add resident
@@ -38,35 +38,37 @@ const Users = () => {
           users
             .filter((user) => clean(user.full_name).includes(clean(search)))
             .map((user) => (
-              <div className="flex justify-between" key={user.id}>
-                {/* TODO: add user image */}
-                <div className="flex items-center gap-4">
-                  {user.image ? (
-                    <img
-                      src={
-                        import.meta.env.VITE_BACKEND_URL +
-                        "/uploads/" +
-                        user.image
-                      }
-                      alt="user"
-                      className="w-10 h-10 rounded-full"
-                    />
-                  ) : (
-                    <User className="w-10 h-10" />
-                  )}
-                  {/* {JSON.stringify(user)} */}
-                  <div className="flex flex-col">
-                    <span>{user.full_name} </span>
-                    <span className="text-sm italic">@{user.username}</span>
+              <Link to={`/users/${user.id}`}>
+                <div
+                  className="flex justify-between cursor-pointer"
+                  key={user.id}
+                >
+                  {/* TODO: add user image */}
+                  <div className="flex items-center gap-4">
+                    {user.image ? (
+                      <img
+                        src={
+                          import.meta.env.VITE_BACKEND_URL +
+                          "/uploads/" +
+                          user.image
+                        }
+                        alt="user"
+                        className="w-10 h-10 rounded-full"
+                      />
+                    ) : (
+                      <User className="w-10 h-10" />
+                    )}
+                    <div className="flex flex-col">
+                      <span>{user.full_name} </span>
+                      <span className="text-sm italic">@{user.username}</span>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <span className="text-slate-500">{user.points} points</span>
+                    <ChevronRight className="w-6 h-6" />
                   </div>
                 </div>
-                <div className="flex items-center gap-2">
-                  <span className="text-slate-500">{user.points} PTS</span>
-                  <Link to={`/users/${user.id}`}>
-                    <ChevronRight className="w-6 h-6" />
-                  </Link>
-                </div>
-              </div>
+              </Link>
             ))}
       </div>
     </>

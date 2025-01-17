bc2df19b0fa847b7dbc0970f9be1d5bde79d384b
zichen-3974 <e0773974@u.nus.edu>
2025-01-17 20:20:16 +0800

    update reports

--- frontend/src/pages/reports/reports.tsx
+++ frontend/src/pages/reports/reports.tsx
@@ -1,17 +1,23 @@
 "use client"
-import { ColumnDef, SortingState, VisibilityState, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
-import * as React from "react";
-import { useEffect, useState } from "react";
-
-import { GetAllOrdersOrdersGetResponse, GetAllProductsProductsGetResponse, MiniOrderPublic, ProductPublic } from "@/api";
+import { MiniOrderPublic, MiniProductPublic, ProductPublic } from "@/api";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table";
 import { getOrders } from "@/features/orders/queries";
 import { getProducts } from "@/features/products/queries";
 import { useCombinedStore } from "@/store/user/user-store-provider";
 import { useQuery } from "@tanstack/react-query";
- 
+import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
 import { format } from "date-fns";
+import * as React from "react";
+import { useEffect, useState } from "react";
+
+import {
+  DropdownMenu, DropdownMenuContent,
+  DropdownMenuItem, DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 
 
 const Reports = () => {
@@ -24,18 +30,15 @@ const Reports = () => {
     return null;
   }
 
-  const isStaff = user?.role !== "resident";
-
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = React.useState({});
   const [toggle, setToggle] = useState("Go to Inventory Summary Report");
   const [dateRange, setDateRange] = useState({ start: format(new Date(new Date().setDate(new Date().getDate() - 7)), "yyyy-MM-dd"), end: format(new Date(), "yyyy-MM-dd"),});
   const [error, setError] = useState<string | null>(null);
   const [stats, setStats] = useState({ claimed: 0, pending: 0, rejected: 0, approved: 0, totalPoints: 0 });
-  type DataType = GetAllOrdersOrdersGetResponse | GetAllProductsProductsGetResponse;
-  const [data, setData] = useState<DataType | undefined>(undefined);
+  const [productStats, setProductStats] = useState({ food: 0, nonfood: 0, special: 0, lowQuantity: 0 });
   const [filteredOrders, setFilteredOrders] = useState<MiniOrderPublic[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<MiniProductPublic[]>([]);
+  const [quantity, setQuantity] = useState(100)
+  const [category, setCategory] = useState("");
 
 
   // -------------------------------- functions for weekly request --------------------------------
@@ -65,10 +68,7 @@ const Reports = () => {
         return createdAt >= startDate && createdAt <= endDate;
       });
   
-      // This will set the state with the filtered orders
       setFilteredOrders(filtered);
-
-      
 
       const claimed = filteredOrders.filter((order) => order.state === "claimed").length;
       const pending = filteredOrders.filter((order) => order.state === "pending").length;
@@ -88,11 +88,64 @@ const Reports = () => {
   };
 
   useEffect(() => {
+    console.log("hi1");
     filterOrders();
   }, [orders, dateRange]);
 
-  // -------------------------------- functions for inventory summary --------------------------------
+  useEffect(() => {
+    console.log("hi2");
+
+    const claimed = orders.filter((order) => order.state === "claimed").length;
+    const pending = orders.filter((order) => order.state === "pending").length;
+    const rejected = orders.filter((order) => order.state === "rejected").length;
+    const approved = orders.filter((order) => order.state === "approved").length;
+
+    const totalPoints = orders.reduce((acc, order) => {
+    const orderProducts = order.order_products;
+        const orderTotalPoints = orderProducts.reduce((total, orderProduct) => {
+          return total + orderProduct.qty * orderProduct.product.points;
+        }, 0);
+        return acc + orderTotalPoints;
+      }, 0);
+
+      setStats({ claimed, pending, rejected, approved, totalPoints });
+  }, []);
+
+  // -------------------------------- functions for inventory summary -------------------------------- //
+  useEffect(() => {
+    console.log("hi3");
+
+    filterProducts();
+  }, [products, category, quantity]);
+
+  const filterProducts = () => {
+    let filtered = products;
+    if (!filtered) { return ""}
+    
+    if (category && category !== 'all') { filtered = filtered.filter(product => product.category === category);}
+    if (quantity) { filtered = filtered.filter(product => product.total_qty <= quantity);}
+
+    setFilteredProducts(filtered);
+
+    setProductStats({
+      food: filtered.filter(product => product.category === 'food').length,
+      nonfood: filtered.filter(product => product.category === 'nonfood').length,
+      special: filtered.filter(product => product.category === 'special').length,
+      lowQuantity: filtered.filter(product => product.total_qty <= quantity).length
+    });
+  };
+
+  const handleCategoryChange = ( category: string ) => {
+    setCategory(category)
+    filterProducts();
+  };
   
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value, 10);
+    if (isNaN(value)) { return; }
+    const numericValue = Math.max(value, 0);
+    setQuantity(numericValue);
+  };
 
   const handleToggle = () => {
     setToggle(toggle === "Go to Inventory Summary Report" 
@@ -110,7 +163,7 @@ const Reports = () => {
         <Button
            variant="outline"
            size="sm"
-           className="bg-blue-500 text-white hover:bg-blue-300 w-[250px]"
+           className="bg-blue-500 text-white hover:bg-blue-300"
            onClick={handleToggle}
          >
          {toggle}
@@ -119,11 +172,11 @@ const Reports = () => {
         {toggle === "Go to Inventory Summary Report" ? (
             <RequestTable data={filteredOrders}/>
         ) : (
-            <InventoryTable data={products}/>
+            <InventoryTable data={filteredProducts}/>
         )}
           
         {toggle === "Go to Inventory Summary Report" && (
-        <div className="flex items-center gap-4 pt-4 pb-12">
+        <div className="flex items-center gap-4 pt-4 pb-12 relative z-10">
           <div>
             <label htmlFor="start" className="block text-sm font-small">
               Start Date:
@@ -151,21 +204,51 @@ const Reports = () => {
             />
           </div>
         </div>
-      )}
-        {toggle === "Go to Inventory Summary Report" ? (
-          <WeeklyRequestSummaryStats stats={stats} />
-        ) : (
-          <InventorySummaryStats stats={stats} />
         )}
+        {toggle !== "Go to Inventory Summary Report" && (
+        <div className="items-center gap-4 pt-4 pb-12">
+          <div className="flex flex-col items-start">
+            <label htmlFor="quantity" className="block text-sm font-small mb-1 pr-4 pt-1">Quantity ≤</label>
+            <Input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={quantity}
+              onChange={handleQuantityChange}
+              className="w-full md:w-auto"
+            />
+          </div>
+          
+          <div className="flex flex-col items-start pt-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">Category: {category || 'Select Category'}</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 bg-white border border-gray-300 rounded shadow-md">
+                <DropdownMenuLabel className="px-4 py-2 font-semibold text-gray-700">Categories</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => handleCategoryChange('food')}>Food</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleCategoryChange('nonfood')}>Non-Food</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleCategoryChange('special')}>Special</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleCategoryChange('all')}>All</DropdownMenuItem>
+                <DropdownMenuSeparator />
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      )}
 
+      {toggle === "Go to Inventory Summary Report" ? (
+      <WeeklyRequestSummaryStats stats={stats} /> ) : ( <InventorySummaryStats stats={productStats} />)}
       </div>
     </div>
   );
 };
 
-    
 export default Reports;
 
+
+// ------------------------------------------------ helpers ------------------------------------------------//
 const RequestColumns: ColumnDef<MiniOrderPublic>[] = [
   {
     accessorKey: "user",
@@ -247,7 +330,7 @@ const RequestTable = ({ data }) => {
   });
 
   return (
-    <div className="w-full h-[30vh]">
+    <div className="w-full">
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -293,7 +376,6 @@ const RequestTable = ({ data }) => {
           </TableBody>
         </Table>
       </div>
-
       <div className="flex items-center justify-between py-4">
         <div className="text-sm text-muted-foreground">
           {table.getRowModel().rows.length} of{" "}
@@ -322,7 +404,6 @@ const RequestTable = ({ data }) => {
   );
 };
 
-
 const InventoryTable = ({ data }) => {
   const table = useReactTable({
     data: data,
@@ -338,7 +419,7 @@ const InventoryTable = ({ data }) => {
   });
 
   return (
-    <div className="w-full h-[30vh]">
+    <div className="w-full">
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -384,7 +465,6 @@ const InventoryTable = ({ data }) => {
           </TableBody>
         </Table>
       </div>
-
       <div className="flex items-center justify-between py-4">
         <div className="text-sm text-muted-foreground">
           {table.getRowModel().rows.length} of{" "}
@@ -408,7 +488,7 @@ const InventoryTable = ({ data }) => {
             Next
           </Button>
         </div>
-      </div>
+      </div>  
     </div>
   );
 };
@@ -452,26 +532,24 @@ const InventorySummaryStats = ({ stats }) => {
       <h2 className="text-xl font-bold mb-2">Summary Statistics</h2>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div className="bg-gray-100 p-4 rounded">
-          <p className="text-sm">Entries Claimed</p>
-          <p className="text-lg font-medium">{stats.claimed}</p>
+          <p className="text-sm">Food Items</p>
+          <p className="text-lg font-medium">{stats.food}</p>
         </div>
         <div className="bg-gray-100 p-4 rounded">
-          <p className="text-sm">Entries Pending</p>
-          <p className="text-lg font-medium">{stats.pending}</p>
+          <p className="text-sm">Non-Food Items</p>
+          <p className="text-lg font-medium">{stats.nonfood}</p>
         </div>
         <div className="bg-gray-100 p-4 rounded">
-          <p className="text-sm">Entries Rejected</p>
-          <p className="text-lg font-medium">{stats.rejected}</p>
+          <p className="text-sm">Special Items</p>
+          <p className="text-lg font-medium">{stats.special}</p>
         </div>
-        <div className="bg-gray-100 p-4 rounded">
-          <p className="text-sm">Entries Approved</p>
-          <p className="text-lg font-medium">{stats.approved}</p>
-        </div>
-        <div className="bg-gray-100 p-4 rounded">
-          <p className="text-sm">Total Points Spent</p>
-          <p className="text-lg font-medium">{stats.totalPoints}</p>
+        <div className="bg-red-100 p-4 rounded">
+          <p className="text-sm text-red-800">Low Quantity Items</p>
+          <p className="text-lg font-medium text-red-800">{stats.lowQuantity}</p>
+          <p className="text-sm text-red-600">Less than or equal 10 in stock!</p>
         </div>
       </div>
     </div>
+
   );
 };
